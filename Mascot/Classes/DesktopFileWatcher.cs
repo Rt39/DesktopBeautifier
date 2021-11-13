f4b70d3de147e7aaebd3b0666258dceb26b076f8
Rt39 <sunrainshr@qq.com>
2021-11-13 21:25:01 +0800

    添加总目录

---
+++ Mascot/Classes/DesktopFileWatcher.cs
@@ -0,0 +1,23 @@
+﻿using Mascot.UtilClass;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascot.Classes {
+    public class DesktopFileWatcher : IDisposable {
+        private static readonly string _settingFilePath = Path.Combine(Utils.Definitions.SettingFolder, "FileWatch", "setting.json");
+
+        private FileSystemWatcher _fileSystemWatcher = new FileSystemWatcher(System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        private FileWatchSettings _fileWatchSettings;
+
+        public event EventHandler<FileSystemEventArgs> NewFile;
+
+
+        public void Dispose() {
+            _fileSystemWatcher.Dispose();
+        }
+    }
+}
---
+++ Mascot/UtilClass/FileWatchSettings.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascot.UtilClass {
+    [Serializable]
+    public struct FileWatchSettings {
+        // 需要忽略的扩展名
+        public string ExtentionExceptions { get; set; }
+        [JsonIgnore]
+        // 需要忽略的扩展名（数组）
+        public string[] ExtentionExceptionList {
+            get { return Array.ConvertAll(ExtentionExceptions.Split(';'), p => p.Trim()); }
+        }
+        // 保存的目录
+        public Dictionary<string, string> ArchiveDirectory { get; set; }
+    }
+}
