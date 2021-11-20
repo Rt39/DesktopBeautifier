572a8ab839b16581c0efbcd8b66f133de83721c0
Rt39 <sunrainshr@qq.com>
2021-11-20 16:47:17 +0800

    修复无法触发的bug

--- Mascot/Classes/DesktopFileWatcher.cs
+++ Mascot/Classes/DesktopFileWatcher.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mascot.Classes {
-    public class DesktopFileWatcher : IDisposable {
+namespace Mascot.Classes
+{
+    public class DesktopFileWatcher : IDisposable
+    {
         private static readonly string _folder = Path.Combine(Utils.Definitions.SettingFolder, "FileWatch");
         private static readonly string _settingFilePath = Path.Combine(_folder, "setting.json");
 
@@ -17,15 +19,18 @@ namespace Mascot.Classes {
 
         public event EventHandler<FileSystemEventArgs> NewFile;
 
-        public DesktopFileWatcher() {
+        public DesktopFileWatcher()
+        {
             if (!Directory.Exists(_folder)) Directory.CreateDirectory(_folder);
-            if (File.Exists(_settingFilePath)) {
+            if (File.Exists(_settingFilePath))
+            {
                 string setting;
                 using (StreamReader sr = new StreamReader(_settingFilePath))
                     setting = sr.ReadToEnd();
                 FileWatchSettings = JsonConvert.DeserializeObject<FileWatchSettings>(setting);
             }
-            else {
+            else
+            {
                 FileWatchSettings = new FileWatchSettings
                 {
                     ExtentionExceptions = ".lnk"
@@ -35,33 +40,39 @@ namespace Mascot.Classes {
             FileWatchSettings.IgnoreFolderChanged += ChangeIgnoreFolder;
 
             _fileSystemWatcher.NotifyFilter = FileWatchSettings.IsIgnoreFolder ? NotifyFilters.FileName : NotifyFilters.FileName | NotifyFilters.DirectoryName;
-            _fileSystemWatcher.Changed += OnCreate;
+            _fileSystemWatcher.Created += OnCreate;
+            _fileSystemWatcher.EnableRaisingEvents = true;
         }
 
         // 过滤不必要的事件
-        private void OnCreate(object sender, FileSystemEventArgs e) {
+        private void OnCreate(object sender, FileSystemEventArgs e)
+        {
             string extention = Path.GetExtension(e.FullPath);
             if (FileWatchSettings.ExtentionExceptionList.Contains(extention)) return;
             NewFile(this, e);
         }
 
         // 修改监听文件夹
-        private void ChangeIgnoreFolder(object sender, EventArgs e) {
+        private void ChangeIgnoreFolder(object sender, EventArgs e)
+        {
             _fileSystemWatcher.NotifyFilter = FileWatchSettings.IsIgnoreFolder ? NotifyFilters.FileName : NotifyFilters.FileName | NotifyFilters.DirectoryName;
         }
 
-        public void MoveToFolder(FileSystemEventArgs e, string destDirPath) {
+        public void MoveToFolder(FileSystemEventArgs e, string destDirPath)
+        {
             Directory.Move(e.FullPath, Path.Combine(destDirPath, e.Name));
         }
 
-        private void Serialize() {
-            using (FileStream fs = new FileStream(_settingFilePath, FileMode.OpenOrCreate | FileMode.Truncate))
+        private void Serialize()
+        {
+            using (FileStream fs = new FileStream(_settingFilePath, FileMode.OpenOrCreate))
             using (StreamWriter sw = new StreamWriter(fs))
             using (JsonWriter writer = new JsonTextWriter(sw))
                 new JsonSerializer().Serialize(writer, FileWatchSettings);
         }
 
-        public void Dispose() {
+        public void Dispose()
+        {
             _fileSystemWatcher.Dispose();
             Serialize();
         }
